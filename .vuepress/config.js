9f12a0303b0418eea81e2863b6e55ab88c937500
Beta <me@pbeta.me>
2019-12-09 01:27:34 +0800

    feat: 添加初始内容,使用Now部署

---
+++ .vuepress/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  title: 'Gatsby 中文文档',
+  description: '基于 React + Typescript + GraphQL 快速搭建网站',
+  themeConfig: {
+    nav: [{ text: 'Home', link: '/' }, { text: 'Guide', link: '/guides/' }]
+  },
+  dest: 'public'
+}
+
